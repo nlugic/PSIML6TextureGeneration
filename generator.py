02ee07f4579792bb58bba1a8ecef4142796b5202
nlugic <nlugic@gmail.com>
2020-08-07 16:02:24 +0200

    fixed generator deconvolution layers

--- generator.py
+++ generator.py
@@ -7,14 +7,22 @@ class SGANGenerator(nn.Module):
         self.name = 'SGAN' + str(num_layers) + '_GEN'
         self.layers = []
         
-        NUM_FILTERS = [2 ** (num_layers - i) for i in range(num_layers)]
+        NUM_FILTERS = [2 ** (num_layers + 4 - i) for i in range(num_layers - 1)] + [3] # 3 -> RGB output
         KERNEL_SIZE = 5
         STRIDE = 2
         PADDING = KERNEL_SIZE // 2
 
         for i in range(num_layers):
             # pitanje paddinga
-            self.layers.append(nn.ConvTranspose2d(in_channels, NUM_FILTERS[i]), kernel_size = KERNEL_SIZE, stride = STRIDE, padding = PADDING)
+            self.layers.append(nn.ConvTranspose2d(
+                in_channels if i == 0 else NUM_FILTERS[i - 1],
+                NUM_FILTERS[i],
+                kernel_size = KERNEL_SIZE,
+                stride = STRIDE,
+                padding = PADDING,
+                output_padding = 1
+            ))
+
             if i < num_layers - 1:
                 # inplace mozda ne bude radio
                 self.layers.append(nn.ReLU(inplace = True))
--- test_generator.py
+++ test_generator.py
@@ -1,9 +1,7 @@
 import torch
-import torch.nn as nn
 from generator import SGANGenerator
 
-x = torch.ones(1, 100, 256, 256)
+x = torch.ones(1, 100, 32, 32)
 gen = SGANGenerator(100, 5)
 y = gen(x)
-print(y)
-print(y.shape)
+print(x.shape, '->', y.shape)
