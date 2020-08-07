16bdd02418612b935dd5b65d28f4c6722406e4ae
nlugic <nlugic@gmail.com>
2020-08-07 18:05:45 +0000

    refactored image loading function

--- helpers.py
+++ helpers.py
@@ -1,10 +1,10 @@
-import os
+import os, random
 import numpy as np
 from PIL import Image
 import torch
 import torch.nn as nn
 
-# treba da se dodaju stvari vezane za device, tako da kad se slike ucitavaju budu odma na GPU
+DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 # uradi se model.apply(init_sgan_weights) da bi se sve tezine u mrezi inicijalizovale
 def init_sgan_weights(node):
@@ -16,11 +16,11 @@ def init_sgan_weights(node):
         nn.init.normal_(node.weight.bias, std = 0.02) # ovo je u kodu najverovatnije 0.0
 
 def image_to_tensor(img):
-    tensor = torch.Tensor(np.array(img).transpose((2, 0, 1)))
+    tensor = torch.Tensor(np.array(img).transpose((2, 0, 1))).to(DEVICE)
     return tensor / 255.0 * 2.0 - 1.0 # u kodu je -1..1 pa sam i ovde tako stavio, mozda treba od 0..1 
 
 def save_tensor_as_image(tensor, filename):
-    img = tensor.numpy().transpose((1, 2, 0)) # treba videti da li treba ovde i gore transpose
+    img = tensor.cpu().numpy().transpose((1, 2, 0)) # treba videti da li treba ovde i gore transpose
     img = np.uint8((img + 1.0) * 255.0 / 2.0)
 
     Image.fromarray(img).save(filename)
@@ -28,28 +28,34 @@ def save_tensor_as_image(tensor, filename):
 def get_train_dataset(path, size = 128, batch_size = 64, mirror = True):
     images_to_sample = []
 
-    for file in os.listdir(path): # promeniti tako da pita dal je fajl
-        name = path + file
-        try:
-            img = Image.open(name)
-            images_to_sample += [image_to_tensor(img)]
-            if mirror:
-                img = img.transpose(Image.FLIP_LEFT_RIGHT)
-                images_to_sample += [image_to_tensor(img)]
-        except:
-            print("Image ", name, " failed to load!")
+    if os.path.isdir(path): # folder sa slikama
+        for file in os.listdir(path): # promeniti tako da pita dal je fajl
+            filename = os.path.join(path, file)
+            try:
+                img = Image.open(filename)
+                images_to_sample.append(image_to_tensor(img))
+                if mirror: # pitanje koliko je ovo potrebno
+                    img = img.transpose(Image.FLIP_LEFT_RIGHT)
+                    images_to_sample.append(image_to_tensor(img))
+            except:
+                print("Image ", filename, " failed to load!")
 
-    while True:
-        train_dataset = Torch.zeros(batch_size, 3, size, size)
-        for i in range(batch_size):
-            image_to_sample = images_to_sample[np.random.randint(len(images_to_sample))]
-            if size < image_to_sample.shape[1] and size < image_to_sample.shape[2]:
-                h = np.random.randint(image_to_sample.shape[1] - size)
-                w = np.random.randint(image_to_sample.shape[2] - size)
-                img = image_to_sample[:, h:h + size, w:w + size]
-            else:
-                img = image_to_sample
+        random.shuffle(images_to_sample)
+    else: # pojedinacna slika
+        images_to_sample.append(image_to_tensor(Image.open(path)))
 
-            train_dataset[i, :, :, :] = img
+    train_dataset = torch.zeros(batch_size, 3, size, size, device = DEVICE)
 
-        return train_dataset
+    for i in range(batch_size):
+        image_to_sample = images_to_sample[i % len(images_to_sample)]
+
+        if size < image_to_sample.shape[1] and size < image_to_sample.shape[2]:
+            h = np.random.randint(image_to_sample.shape[1] - size)
+            w = np.random.randint(image_to_sample.shape[2] - size)
+            img = image_to_sample[:, h:h + size, w:w + size]
+        else:
+            img = image_to_sample
+
+        train_dataset[i, :, :, :] = img
+
+    return train_dataset
