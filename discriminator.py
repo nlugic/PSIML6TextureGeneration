fixed output_padding and batch normalization order

discriminator.py
@@ -22,9 +22,9 @@ class SGANDiscrimantor(nn.Module):
                 ))
                 
             if i < num_layers - 1:
-                self.layers.append(nn.LeakyReLU(0.2, inplace = True))
                 if i > 0:
                     self.layers.append(nn.BatchNorm2d(NUM_FILTERS[i], eps = 1e-04, momentum = 0.1))
+                self.layers.append(nn.LeakyReLU(0.2, inplace = True))
 
     def forward(self, input_tensor):
         output = input_tensor

generator.py
@@ -19,12 +19,12 @@ class SGANGenerator(nn.Module):
                 kernel_size = KERNEL_SIZE,
                 stride = STRIDE,
                 padding = PADDING,
-                output_padding = 1
+                #output_padding=1
             ))
 
             if i < num_layers - 1:
-                self.layers.append(nn.ReLU(inplace = True))
                 self.layers.append(nn.BatchNorm2d(NUM_FILTERS[i], eps = 1e-04, momentum = 0.1))
+                self.layers.append(nn.ReLU(inplace = True))
             else:
                 # za poslednji sloj rade tanh bez batch norma
                 self.layers.append(nn.Tanh())

train.py
@@ -21,7 +21,7 @@ dis = SGANDiscrimantor(layers).to(device)
 dis.layers.apply(init_sgan_weights)
 
 dataset_folder = "train_textures/"
-dataset_iter = get_train_dataset(dataset_folder, device, size=l * 2**layers, batch_size=batch_size)
+dataset_iter = get_train_dataset(dataset_folder, device, size=(l-1) * 2**layers + 1, batch_size=batch_size)
 
 loss_funct_g = lambda pred: torch.mean(F.binary_cross_entropy_with_logits(pred, pred.new_ones(pred.size())))
 loss_funct_d = lambda pred_real, pred_fake: torch.mean(F.binary_cross_entropy_with_logits(pred_fake, pred_fake.new_zeros(pred_fake.size()))) + torch.mean(F.binary_cross_entropy_with_logits(pred_real, pred_real.new_ones(pred_real.size())))
@@ -71,5 +71,5 @@ while True:
 
     writer.add_scalar("Loss/Generator", np.mean(loss_list_g), epoch)
     writer.add_scalar("Loss/Discriminator", np.mean(loss_list_d), epoch)
-    writer.add_images("Generated images/l9", gen(z9)[:4] / 2 + 0.5, epoch)
-    writer.add_images("Generated images/l20", gen(z20)[:4] / 2 + 0.5, epoch)
+    writer.add_images("Generated images/l9", gen(z9).detach()[:4] / 2 + 0.5, epoch)
+    writer.add_images("Generated images/l20", gen(z20).detach()[:4] / 2 + 0.5, epoch)