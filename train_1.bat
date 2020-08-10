d5b170a1fb9878d0f2967c6cb624737dd27fb33b
nlugic <nlugic@gmail.com>
2020-08-10 10:32:01 +0000

    added most of the trained models

--- train_1.bat
+++ train_1.bat
@@ -2,8 +2,8 @@ python ./train.py ./textures/barcelona.jpg barcelona -o ./trained_models/ -d 1
 python ./train.py ./textures/flowers_0.jpg flowers_0 -o ./trained_models/ -d 1
 python ./train.py ./textures/grass_0.jpg grass_0 -o ./trained_models/ -d 1
 python ./train.py ./textures/leaves_0.jpg leaves_0 -o ./trained_models/ -d 1
-python ./train.py ./textures/stone/stone_1.jpg stone_1 -o ./trained_models/ -d 1
-python ./train.py ./textures/fabric/fabric_2.jpg fabric_2 -o ./trained_models/ -d 1
-python ./train.py ./textures/cork/ cork -o ./trained_models/ -d 1
-python ./train.py ./textures/aluminium_foil/ aluminium_foil -o ./trained_models/ -d 1
-python ./train.py ./textures/brown_bread/ brown_bread -o ./trained_models/ -d 1
+python ./train.py ./textures/stone/stone_1.jpg stone_1 -o ./trained_models/ -s 9 -d 1
+python ./train.py ./textures/fabric/fabric_2.jpg fabric_2 -o ./trained_models/ -s 9 -d 1
+python ./train.py ./textures/cork/ cork -o ./trained_models/ -s 9 -d 1
+python ./train.py ./textures/aluminium_foil/ aluminium_foil -o ./trained_models/ -s 9 -d 1
+python ./train.py ./textures/brown_bread/ brown_bread -o ./trained_models/ -s 9 -d 1
