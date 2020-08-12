9e55c918a37e58796570e7297d3318bc7b4cbf69
nlugic <nlugic@gmail.com>
2020-08-13 01:34:32 +0200

    fixed generate script, now all arguments make sense

--- generate.py
+++ generate.py
@@ -18,48 +18,54 @@ gen.eval()
 
 input_height = args.input_size + (4 if args.tiling_rows > 0 else 0)
 input_width = args.input_size + (4 if args.tiling_columns > 0 else 0)
-input_tensor = torch.rand(1, input_channels, input_height, input_width).to(H.DEVICE) * 2.0 - 1.0
+input_tensor = torch.rand(1, input_channels, input_width, input_height).to(H.DEVICE) * 2.0 - 1.0
 
 if args.tiling_rows > 0:
-    input_tensor[:, :, :4, :] = input_tensor[:, :, -4:, :]
+    input_tensor[:, :, :, :4] = input_tensor[:, :, :, -4:]
 
 if args.tiling_columns > 0:
-    input_tensor[:, :, :, :4] = input_tensor[:, :, :, -4:]
+    input_tensor[:, :, :4, :] = input_tensor[:, :, -4:, :]
 
 with torch.no_grad():
     texture = gen(input_tensor).cpu() / 2.0 + 0.5
 
 if args.tiling_rows > 0 or args.tiling_columns > 0:
-    best = 1e6
-    crop1 = 0
-    crop2 = 0
+    best_loss = float('inf')
+    crop_left_top = 0
+    crop_right_bottom = 0
+
     for i in range(2 ** args.sgan_layers, 2 ** (args.sgan_layers + 1)):
         for j in range(2 ** args.sgan_layers, 2 ** (args.sgan_layers + 1)):
-            loss = (torch.abs(texture[:, :, :, i] - texture[:, :, :, -j]).mean() if args.tiling_columns > 0 else 0) \
-                 + (torch.abs(texture[:, :, i] - texture[:, :, -j]).mean().item() if args.tiling_rows > 0 else 0)
-            if loss < best:
-                best = loss
-                crop1 = i
-                crop2 = j
+            loss = (torch.mean(torch.abs(texture[:, :, i, :] - texture[:, :, -j, :])) if args.tiling_columns > 0 else 0) \
+                 + (torch.mean(torch.abs(texture[:, :, :, i] - texture[:, :, :, -j])) if args.tiling_rows > 0 else 0)
 
-if args.tiling_rows > 0:
-    texture = texture[:, :, crop1:-crop2, :]
+            if loss < best_loss:
+                best_loss = loss
+                crop_left_top = i
+                crop_right_bottom = j
 
-if args.tiling_columns > 0:
-    texture = texture[:, :, :, crop1:-crop2]
+    if args.tiling_rows > 0:
+        texture = texture[:, :, :, crop_left_top:-crop_right_bottom]
+
+    if args.tiling_columns > 0:
+        texture = texture[:, :, crop_left_top:-crop_right_bottom, :]
 
 if args.tiling_rows > 1:
+    initial_texture = texture
+
     for i in range(args.tiling_rows - 1):
-        texture = torch.cat((texture, texture), 2)
+        texture = torch.cat((texture, initial_texture), 3)
 
 if args.tiling_columns > 1:
+    initial_texture = texture
+
     for j in range(args.tiling_columns - 1):
-        texture = torch.cat((texture, texture), 3)
+        texture = torch.cat((texture, initial_texture), 2)
 
 texture = texture[0, :, :, :].numpy().T
 
 if args.output_path:
-    img = Image.fromarray(np.uint8(texture * 255.0))
+    img = Image.fromarray(np.uint8(texture * 255.0)).rotate(90.0)
     img.save(os.path.join(args.output_path, os.path.basename(args.saved_model_path)) + '_output.jpg')
 else:
     plt.title('Texture image generated with SGAN' + str(args.sgan_layers) + ':')
